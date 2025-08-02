f03323c35bd7c2da15aa2ffb99b2c4b5e45a9288
hailm <hai.lemanh.1310@gmail.com>
2025-08-02 11:24:11 +0700

    feat: page tuvanphapluat

--- tuvanphapluat.php
+++ tuvanphapluat.php
@@ -18,7 +18,89 @@
 </div>
 <div class="uk-section-small">
     <div class="uk-container">
-
+        <div class="uk-margin-medium-bottom uk-background-cover uk-border-rounded uk-position-relative" style="height: 166px;" data-src="images/banner1.png" uk-img>
+            <div class="uk-position-center-right fz-29 barlow-semibold uk-text-uppercase text-2756A6 tuvanphapluat__box1 uk-border-pill bg-FFF">
+                AI trợ lý ảo hỗ trợ
+            </div>
+        </div>
+        <div class="home__news__head">
+            <div class="uk-child-width-auto" uk-grid>
+                <div>
+                    <div class="home__news__head__line">
+                        <div class="uk-grid-10 uk-flex-middle" uk-grid>
+                            <div>
+                                <h2 class="text-2D2D2D fz-21 playfair-display-900 uk-text-uppercase">Hỏi đáp</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="uk-child-width-1-2@l uk-grid-small uk-grid-20-l uk-margin-top" uk-grid>
+            <?php for ($i = 0; $i < 6; $i++) { ?>
+            <div>
+                <div class="uk-border-rounded">
+                    <div class="padding-12 bg-D7EBFD">
+                        <div class="uk-flex-middle uk-grid-small" uk-grid>
+                            <div>
+                                <div class="uk-grid-5 uk-flex-middle" uk-grid>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                            <path d="M12 2C16.69 2 20.5 5.81 20.5 10.5C20.5 15 17.36 19.66 12.5 22V19H12C7.31 19 3.5 15.19 3.5 10.5C3.5 5.81 7.31 2 12 2ZM11 16.5H13V14.5H11V16.5ZM12.5996 4.04004C10.5398 3.74025 8.71989 5.01019 8.16992 6.83008C8 7.41005 8.44007 8 9.04004 8H9.24023C9.65013 7.99989 9.98014 7.70954 10.1201 7.34961C10.4402 6.45982 11.3801 5.85035 12.4199 6.07031C13.3799 6.27031 14.0802 7.2202 13.9902 8.2002C13.8801 9.44004 12.5099 9.77996 11.6699 10.8799L11.6602 10.8701C11.6302 10.9001 11.6101 10.9502 11.5801 10.9902C11.5502 11.04 11.5104 11.0898 11.4805 11.1396C11.3905 11.2796 11.3198 11.4201 11.2598 11.5801C11.2498 11.6199 11.22 11.6399 11.21 11.6797C11.2 11.6897 11.2002 11.7 11.2002 11.71C11.0802 12.0699 11 12.4999 11 13.0098H13.0098C13.0098 12.7599 13.0404 12.54 13.1104 12.3301C13.1304 12.2601 13.1604 12.1901 13.1904 12.1201C13.2004 12.0801 13.2105 12.0398 13.2305 12.0098C13.2704 11.9299 13.3202 11.8494 13.3701 11.7695C13.38 11.7497 13.3905 11.7297 13.4004 11.7197C14.3005 10.4 16.3696 9.80984 15.9297 7.37012C15.6397 5.68012 14.2896 4.29004 12.5996 4.04004Z" fill="#ED3037"/>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        <div>10/06/2025</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-grid-5 uk-flex-middle" uk-grid>
+                                    <div>
+                                        <div class="text-2756A6 barlow-medium lh-125">Quy định về chế độ tai nạn lao động</div>
+                                    </div>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
+                                            <g clip-path="url(#clip0_4547_296)">
+                                                <path d="M2.35044 4.24805C2.63216 6.24146 3.06943 7.75269 3.80923 8.31055C3.82065 7.64149 3.89197 6.97478 4.02236 6.31836C4.44983 4.18701 5.13697 4.07227 5.79839 2.49756C6.13327 1.70869 6.20916 0.834462 6.01518 0C8.38526 0.408936 10.5863 3.96973 11.1387 8.49731C11.7406 7.72482 12.0926 6.78823 12.148 5.81177C13.194 6.87744 13.7121 8.14575 13.9056 9.53735C14.2976 12.2021 13.477 13.9319 11.3077 14.6118C11.7903 13.5132 11.6752 12.6587 11.173 11.5283C10.9718 11.0662 10.6846 10.6462 10.3266 10.2905C10.1723 11.3892 9.93347 12.2131 9.52682 12.5195C9.51858 12.1504 9.47759 11.7828 9.40433 11.4209C9.16916 10.2515 8.79191 10.1892 8.42446 9.32495C8.23939 8.89236 8.19652 8.41248 8.30197 7.9541C7.00241 8.17871 5.79471 10.1331 5.48482 12.6172C5.15525 12.1935 4.96223 11.6802 4.93119 11.145C4.39247 11.7022 4.05321 12.4209 3.96601 13.1897C3.85823 13.9221 3.91825 14.5325 4.15587 14.9951C2.48762 14.8047 1.36934 13.8782 0.662602 12.3828C-0.352796 10.2417 -0.171518 8.64502 0.785087 6.50513C1.15932 5.66108 1.69045 4.89525 2.35044 4.24805Z" fill="#E52B2F"/>
+                                            </g>
+                                            <defs>
+                                                <clipPath id="clip0_4547_296">
+                                                    <rect width="14" height="15" fill="white"/>
+                                                </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-padding-small bg-F9F9F9">
+                        <div class="uk-grid-5" uk-grid>
+                            <div class="uk-width-auto">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19" fill="none">
+                                    <path d="M15.8333 1.58333C16.704 1.58333 17.4161 2.29565 17.4163 3.16634V12.6663C17.4163 13.5372 16.7041 14.2503 15.8333 14.2503H4.75024L1.58325 17.4163V3.16634C1.58343 2.29576 2.29568 1.58351 3.16626 1.58333H15.8333ZM8.70825 10.2913V11.8753H10.2913V10.2913H8.70825ZM9.49927 3.95833C9.06404 3.95855 8.70825 4.31504 8.70825 4.75032V7.91634C8.70825 8.35162 9.06404 8.70811 9.49927 8.70833C9.93468 8.70833 10.2913 8.35175 10.2913 7.91634V4.75032C10.2913 4.3149 9.93468 3.95833 9.49927 3.95833Z" fill="#2756A6"/>
+                                </svg>
+                            </div>
+                            <div class="uk-width-expand">
+                                <div>Thông tư số 04/2015/TT-BLĐTBXH ngày 2/2/2015 hướng dẫn thực hiện chế độ bồi thường, trợ cấp và chi phí y tế của NSDLĐ đối với NLĐ bị TNLĐ BNN (thay thế Thông tư số 10/2003/TT-BLĐTBXH ngày 18/4/2003 của Bộ LĐ-TB và XH hướng dẫn việc thực hiện chế...</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+        <div class="uk-text-center uk-margin-top">
+            <a href="" class="uk-button uk-button-primary uk-border-pill fz-16 barlow-semibold" style="text-transform: inherit">
+                <div class="uk-flex uk-flex-middle">
+                    <span class="uk-margin-small-right">Xem thêm</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="11" viewBox="0 0 18 11" fill="none">
+                        <path d="M17.8085 6.20774C18.0657 5.95729 18.0712 5.54577 17.8207 5.28858L13.7395 1.09742C13.489 0.840228 13.0775 0.834761 12.8203 1.08521C12.5631 1.33565 12.5577 1.74717 12.8081 2.00436L16.4359 5.72984L12.7104 9.35763C12.4532 9.60807 12.4478 10.0196 12.6982 10.2768C12.9487 10.534 13.3602 10.5394 13.6174 10.289L17.8085 6.20774ZM4.17233e-05 5.51147L-0.00859361 6.16142L17.3464 6.392L17.3551 5.74206L17.3637 5.09211L0.00867705 4.86153L4.17233e-05 5.51147Z" fill="white"/>
+                    </svg>
+                </div>
+            </a>
+        </div>
     </div>
 </div>
 <div class="uk-section-small bg-F3FBFF">
@@ -113,6 +195,16 @@
                 <?php endforeach; ?>
                 </tbody>
             </table>
+        </div>
+        <div class="uk-text-center uk-margin-top">
+            <a href="" class="uk-button uk-button-primary uk-border-pill fz-16 barlow-semibold" style="text-transform: inherit">
+                <div class="uk-flex uk-flex-middle">
+                    <span class="uk-margin-small-right">Xem thêm</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="11" viewBox="0 0 18 11" fill="none">
+                        <path d="M17.8085 6.20774C18.0657 5.95729 18.0712 5.54577 17.8207 5.28858L13.7395 1.09742C13.489 0.840228 13.0775 0.834761 12.8203 1.08521C12.5631 1.33565 12.5577 1.74717 12.8081 2.00436L16.4359 5.72984L12.7104 9.35763C12.4532 9.60807 12.4478 10.0196 12.6982 10.2768C12.9487 10.534 13.3602 10.5394 13.6174 10.289L17.8085 6.20774ZM4.17233e-05 5.51147L-0.00859361 6.16142L17.3464 6.392L17.3551 5.74206L17.3637 5.09211L0.00867705 4.86153L4.17233e-05 5.51147Z" fill="white"/>
+                    </svg>
+                </div>
+            </a>
         </div>
     </div>
 </div>
